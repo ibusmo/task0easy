27276a8ad8790b6f4d4fe5477b15eab669c0461e
ibusmo <omsubusmo@gmail.com>
2011-08-16 05:49:20 +0700

    add 0001.c to new project

---
+++ 0001.c
@@ -0,0 +1,20 @@
+#in//0001 : Grading
+#include"stdio.h"
+
+main()
+{
+	int a,b,c,d;
+	scanf("%d %d %d",&a,&b,&c);
+	d=a+b+c;
+
+	if(d>79) printf("A");
+	else if(d>74) printf("B+");
+	else if(d>69) printf("B");
+	else if(d>64) printf("C+");
+	else if(d>59) printf("C");
+	else if(d>54) printf("D+");
+	else if(d>49) printf("D");
+	else printf("F");
+	printf("\n");
+	
+}
